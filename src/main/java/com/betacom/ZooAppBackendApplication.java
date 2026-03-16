69a4731ff401c64f92a7222880166f7e20f53d6a
safetyon9 <safetyon99@gmaiil.com>
2026-03-16 14:48:27 +0100

    progetto vuoto avviato con eclipse spring boot

---
+++ src/main/java/com/betacom/ZooAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.betacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betacom/ZooAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
