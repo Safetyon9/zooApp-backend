8a4071d5a34fa141293988abf812c5c15a5a5c12
carlo <carlogalli05@gmail.com>
2026-03-18 16:52:21 +0100

    commit preparazione package

---
+++ src/main/java/com/betacom/exceptions/ZooException.java
@@ -0,0 +1,12 @@
+package com.betacom.exceptions;
+
+public class ZooException extends RuntimeException{
+
+	public ZooException() {
+		super();	
+	}
+	
+	public ZooException(String message) {
+		super(message);
+	}
+}
