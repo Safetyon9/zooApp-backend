024bb2102d3daea7252baed2bc69d4a1206668b3
safetyon9 <safetyon99@gmaiil.com>
2026-03-19 15:01:53 +0100

    Aggiunto git ignore

--- src/main/java/com/betacom/persistence/entity/commerce/Clienti.java
+++ src/main/java/com/betacom/persistence/entity/commerce/Clienti.java
@@ -1,10 +1,14 @@
 package com.betacom.persistence.entity.commerce;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -33,4 +37,8 @@ public class Clienti {
 	
 	@Column (nullable =false)
 	private String indirizzo;	
+	
+	@OneToMany
+	@JoinColumn(name = "ordine",referencedColumnName ="id")
+	private List<Ordini> ordine;
 }
---
+++ src/main/java/com/betacom/persistence/entity/gestione/animali/Tartarughe.java
@@ -0,0 +1,34 @@
+package com.betacom.persistence.entity.gestione.animali;
+
+import com.betacom.persistence.entity.gestione.Animali;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tartarughe")
+public class Tartarughe extends Animali{
+
+
+	@Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+	
+	@Column(nullable = false)
+	private String tipo; //terra,acqua,mare
+	
+	@Column(nullable = false)
+	private Boolean ovipara; //true fa le uova false no
+	
+	@Column(nullable = false)
+	private Boolean letargo; //true e in letargo false sveglia
+	
+}
