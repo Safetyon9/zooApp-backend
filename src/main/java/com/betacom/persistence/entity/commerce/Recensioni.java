3832d806c5add1028825f7db2ffa6efbde3c9414
safetyon9 <safetyon99@gmaiil.com>
2026-03-20 00:18:58 +0100

    okay ho visto il database credo andrò a dormire

--- src/main/java/com/betacom/persistence/entity/commerce/Recensioni.java
+++ src/main/java/com/betacom/persistence/entity/commerce/Recensioni.java
@@ -1,17 +1,7 @@
 package com.betacom.persistence.entity.commerce;
 
 import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,43 +11,36 @@ import lombok.Setter;
 @Table(
     name = "recensioni",
     uniqueConstraints = {
-    	@UniqueConstraint(columnNames = {"utente_id", "acquistabile_id"}),
-        @UniqueConstraint(columnNames = {"utente_id", "generale_zoo"})
+        @UniqueConstraint(columnNames = {"cliente_id", "items_id"}),
+        @UniqueConstraint(columnNames = {"cliente_id", "generale_zoo"})
     }
 )
 public class Recensioni {
 
-	@Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-	
-	
-	@Column (nullable = false)
-	private Integer voto; // 1-5 
-	
-	@Column (nullable = false, columnDefinition = "TEXT")
-	private String testo;
-	
-	@Column (nullable = false, length = 50)
-	private String titolo;
-	
-	@Column (nullable = false)
-	private LocalDateTime dataCreazione = LocalDateTime.now();
-	
-	@Column(nullable = false)
-	private Boolean generaleZoo = false;
 
-	@ManyToOne(
-			fetch = FetchType.LAZY,
-			optional = false
-			)
-	@JoinColumn(name = "cliente_id")
-	private Clienti cliente;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "items_id")
-	private Items item;
-	
-	
-	
+    @Column(nullable = false)
+    private Integer voto; // 1-5
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String testo;
+
+    @Column(nullable = false, length = 50)
+    private String titolo;
+
+    @Column(nullable = false)
+    private LocalDateTime dataCreazione = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private Boolean generaleZoo = false;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Clienti cliente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "items_id")
+    private Items item;
 }
