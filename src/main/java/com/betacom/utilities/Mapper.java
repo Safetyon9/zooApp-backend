89462e8cc1283e08bc42d31618c5ac592fcd323d
safetyon9 <safetyon99@gmaiil.com>
2026-03-23 16:26:11 +0100

    anti depressive per luca

--- src/main/java/com/betacom/utilities/Mapper.java
+++ src/main/java/com/betacom/utilities/Mapper.java
@@ -1,6 +1,8 @@
 package com.betacom.utilities;
 
+import com.betacom.dto.outputs.commerce.checkout.OggettiOrdiniDTO;
 import com.betacom.dto.outputs.commerce.items.BigliettiDTO;
+import com.betacom.persistence.entity.commerce.checkout.OggettiOrdini;
 import com.betacom.persistence.entity.commerce.items.Biglietti;
 
 public class Mapper {
@@ -16,4 +18,15 @@ public class Mapper {
 	            .tipo(b.getTipo())
 	            .build();
 	}
+	
+	private OggettiOrdiniDTO buildOgettiOrdiniDTO (OggettiOrdini oo) {
+        return OggettiOrdiniDTO.builder()
+                .id(oo.getId())
+                .itemId(oo.getItem().getId())
+                .nomeItem(oo.getItem().getNome())
+                .quantita(oo.getQuantita())
+                .prezzoUnitario(oo.getPrezzoUnitario())
+                .prezzoTotale(oo.getPrezzoTotale())
+                .build();
+    }
 }
