0b26965e7ec148c112a95b755dc39d38dac498b2
Luca <luca@MacBook-Air-di-Luca.local>
2026-03-24 11:49:47 +0100

    controller utenti e ordini

--- src/main/java/com/betacom/controllers/commerce/UtentiController.java
+++ src/main/java/com/betacom/controllers/commerce/UtentiController.java
@@ -1,5 +1,100 @@
 package com.betacom.controllers.commerce;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betacom.dto.inputs.UtentiReq;
+import com.betacom.response.Resp;
+import com.betacom.services.interfaces.IMessaggiServices;
+import com.betacom.services.interfaces.IUtentiServices;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("rest/utente")
 public class UtentiController {
 
+	private final IUtentiServices utS;
+	private final IMessaggiServices msgS;
+	
+	@PostMapping("/create")
+	public ResponseEntity<Resp> create(@RequestBody(required = true)  UtentiReq req){
+		Resp r = new Resp();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			utS.create(req);
+			r.setMsg(msgS.get("rest_created"));
+		} catch (Exception e) {
+			r.setMsg(e.getMessage());
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);		
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Resp> update(@RequestBody(required = true)  UtentiReq req){
+		Resp r = new Resp();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			utS.update(req);
+			r.setMsg(msgS.get("rest_updated"));
+		} catch (Exception e) {
+			log.debug("Error:" + e.getMessage());
+			r.setMsg(e.getMessage());
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);		
+	}
+
+	@DeleteMapping("/delete/{userName}")
+	public ResponseEntity<Resp> delete(@PathVariable(required = true)  String userName){
+		Resp r = new Resp();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			utS.delete(userName);
+			r.setMsg(msgS.get("rest_deleted"));
+		} catch (Exception e) {
+			r.setMsg(e.getMessage());
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);		
+	}
+
+	@GetMapping("/list")
+	public ResponseEntity<Object> list(){
+		Object r = new Object();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			r= utS.list();
+		} catch (Exception e) {
+			r=e.getMessage();
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);
+	}
+
+	@GetMapping("/findByUserName")
+	public ResponseEntity<Object> findByUserName(@RequestParam (required = true)  String userName){
+		Object r = new Object();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			r= utS.getByUserName(userName);
+		} catch (Exception e) {
+			r=e.getMessage();
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);
+	}
 }
--- src/main/java/com/betacom/controllers/commerce/checkout/OrdiniController.java
+++ src/main/java/com/betacom/controllers/commerce/checkout/OrdiniController.java
@@ -1,5 +1,100 @@
 package com.betacom.controllers.commerce.checkout;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betacom.dto.inputs.commerce.checkout.OrdiniReq;
+import com.betacom.response.Resp;
+import com.betacom.services.interfaces.IMessaggiServices;
+import com.betacom.services.interfaces.commerce.checkout.IOrdiniServices;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("rest/ordine")
 public class OrdiniController {
 
+    private final IOrdiniServices ordS;
+    private final IMessaggiServices msgS;
+
+    @PostMapping("/create")
+    public ResponseEntity<Resp> create(@RequestBody(required = true) OrdiniReq req) {
+        Resp r = new Resp();
+        HttpStatus status = HttpStatus.OK;
+        try {
+            ordS.create(req);
+            r.setMsg(msgS.get("rest_created"));
+        } catch (Exception e) {
+            r.setMsg(e.getMessage());
+            status = HttpStatus.BAD_REQUEST;
+        }
+        return ResponseEntity.status(status).body(r);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Resp> update(@RequestBody(required = true) OrdiniReq req) {
+        Resp r = new Resp();
+        HttpStatus status = HttpStatus.OK;
+        try {
+            ordS.update(req);
+            r.setMsg(msgS.get("rest_updated"));
+        } catch (Exception e) {
+            log.debug("Error:" + e.getMessage());
+            r.setMsg(e.getMessage());
+            status = HttpStatus.BAD_REQUEST;
+        }
+        return ResponseEntity.status(status).body(r);
+    }
+
+    @DeleteMapping("/delete/{idOrdine}")
+    public ResponseEntity<Resp> delete(@PathVariable(required = true) Integer idOrdine) {
+        Resp r = new Resp();
+        HttpStatus status = HttpStatus.OK;
+        try {
+            ordS.delete(idOrdine);
+            r.setMsg(msgS.get("rest_deleted"));
+        } catch (Exception e) {
+            r.setMsg(e.getMessage());
+            status = HttpStatus.BAD_REQUEST;
+        }
+        return ResponseEntity.status(status).body(r);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Object> list() {
+        Object r = new Object();
+        HttpStatus status = HttpStatus.OK;
+        try {
+            r = ordS.list();
+        } catch (Exception e) {
+            r = e.getMessage();
+            status = HttpStatus.BAD_REQUEST;
+        }
+        return ResponseEntity.status(status).body(r);
+    }
+
+    @GetMapping("/findById")
+    public ResponseEntity<Object> findById(@RequestParam(required = true) Integer idOrdine) {
+        Object r = new Object();
+        HttpStatus status = HttpStatus.OK;
+        try {
+            r = ordS.getById(idOrdine);
+        } catch (Exception e) {
+            r = e.getMessage();
+            status = HttpStatus.BAD_REQUEST;
+        }
+        return ResponseEntity.status(status).body(r);
+    }
 }
