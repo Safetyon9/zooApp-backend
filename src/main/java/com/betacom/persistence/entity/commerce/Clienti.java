7a13f87cc31293dcdd6112f7f7c4c46d5ceb236f
Safetyon9 <safetyon99@gmail.com>
2026-03-24 14:52:04 +0100

    porca paletta

--- src/main/java/com/betacom/persistence/entity/commerce/Clienti.java
+++ src/main/java/com/betacom/persistence/entity/commerce/Clienti.java
@@ -30,9 +30,6 @@ public class Clienti {
 	@Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
-    
-	@Column (nullable =false, unique = true)
-	private String email;
 	
 	@Column (nullable =false)
 	private String nome;
