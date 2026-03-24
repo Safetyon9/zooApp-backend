3eb78383eb6b2afafe9fa349617b790053d86e25
Ramona <riolo628@gmail.com>
2026-03-24 12:18:18 +0100

    controller

---
+++ src/main/java/com/betacom/services/implementations/gestione/MessaggiImpl.java
@@ -0,0 +1,16 @@
+package com.betacom.services.implementations.gestione;
+
+import org.springframework.stereotype.Service;
+
+import com.betacom.services.interfaces.IMessaggiServices;
+	
+	@Service
+	public class MessaggiImpl implements IMessaggiServices {
+
+	    @Override
+	    public String get(String code) {
+	        
+	        return "Messaggio per codice: " + code;
+	    }
+	}
+
