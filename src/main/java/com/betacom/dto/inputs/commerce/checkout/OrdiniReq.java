3f8445d815cb252e83e4af9b1d4d41b139efc737
Safetyon9 <safetyon99@gmail.com>
2026-03-24 15:09:04 +0100

    porca paletta

--- src/main/java/com/betacom/dto/inputs/commerce/checkout/OrdiniReq.java
+++ src/main/java/com/betacom/dto/inputs/commerce/checkout/OrdiniReq.java
@@ -1,7 +1,5 @@
 package com.betacom.dto.inputs.commerce.checkout;
 
-import java.util.List;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,10 +8,8 @@ import lombok.ToString;
 @Setter
 @ToString
 public class OrdiniReq {
-
     private Integer id;                 // usato per update
     private Integer clienteId;
-    private String indirizzo;           // aggiornabile
-    private List<OggettiOrdiniReq> oggetti;
+    private String indirizzo;
 
 }
