cfec2a21f99b91feee6e03fc3d994877efb500d5
Ramona <riolo628@gmail.com>
2026-03-24 11:28:45 +0100

    controller vuoti

---
+++ src/main/java/com/betacom/controllers/commerce/items/BigliettiController.java
@@ -0,0 +1,15 @@
+package com.betacom.controllers.commerce.items;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/rest/biglietti")
+
+public class BigliettiController {
+
+}
---
+++ src/main/java/com/betacom/controllers/commerce/items/ClientiController.java
@@ -0,0 +1,14 @@
+package com.betacom.controllers.commerce.items;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/rest/clienti")
+public class ClientiController {
+
+}
