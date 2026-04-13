cfce889db1abe1bced02878c8290f4f99315e2ad
Luca <luca@MacBook-Air-di-Luca.local>
2026-04-13 10:37:19 +0200

    TEST JUNIT ERROR

---
+++ src/test/java/com/betacom/mail/MailControllerTest.java
@@ -0,0 +1,15 @@
+package com.betacom.mail;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MailControllerTest {
+	
+}
